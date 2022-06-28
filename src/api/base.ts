f0d1b8aad9587e5edefb01b39c34a1a51904bcd5
csdlmysql <csdlmysql@gmail.com>
2022-06-28 21:24:22 +0700

    fix

--- src/api/base.ts
+++ src/api/base.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://api.fsggroup.io/',
-  baseURL: 'http://0.0.0.0:5001',
+  baseURL: 'http://192.168.1.100:5001',
   timeout: 3600 * 1000 * 3,
   headers: { "Access-Control-Allow-Origin": "*",
             'Content-Type': 'application/json;charset=UTF-8'
