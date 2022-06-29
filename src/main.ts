23abf28688c04e2e2ebddb4eba0c824171911e0b
csdlmysql <csdlmysql@gmail.com>
2022-06-29 19:03:27 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import "./assets/homepage.css"
-import VueSocketIO from 'vue-socket.io'
+import VueSocketIO from 'vue-3-socket.io'
 import SocketIO from 'socket.io-client'
-const socketConnection = SocketIO('http://210.245.54.243:5002/');
-
 const app = createApp(App)
 
 app.use(router)
 app.use(new VueSocketIO({
     debug: false,
-    connection: socketConnection, //options object is Optional
+    connection: SocketIO('http://210.245.54.243:5002/', ), //options object is Optional
   })
 );
 
