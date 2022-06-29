1247627b4412f4f84076d96db4daf726b38f5453
csdlmysql <csdlmysql@gmail.com>
2022-06-29 15:53:09 +0700

    fix

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -46,87 +46,24 @@
         <div class="container-fluid mt-3">
             <h4>__________ TẤT CẢ SẢN PHẨM __________</h4><br>
             <div style="text-align: center; padding-bottom: 30px;"><br>
-                <button type="button" class="btn btn-secondary">Tất cả</button>
-                <button type="button" class="btn btn-secondary">T-shirt</button>
-                <button type="button" class="btn btn-secondary">Hoodie</button>
-                <button type="button" class="btn btn-secondary">Quần</button>
-                <button type="button" class="btn btn-secondary">Phụ kiện</button>
-                <button type="button" class="btn btn-secondary">Áo khoác</button>
+                <button @click="get_all" type="button" class="btn btn-secondary">Tất cả</button>
+                <button @click="get_tshirt" type="button" class="btn btn-secondary">T-shirt</button>
+                <button @click="get_hoodie" type="button" class="btn btn-secondary">Hoodie</button>
+                <button @click="get_quan" type="button" class="btn btn-secondary">Quần</button>
+                <button @click="get_phukien" type="button" class="btn btn-secondary">Phụ kiện</button>
+                <button @click="get_aokhoac" type="button" class="btn btn-secondary">Áo khoác</button>
             </div>
             <div class="row">
-                <div class="col-sm-3 p-3 bg-white">
-                    <div class="thumbnail">
-                        <img src="img/Sweater Tiedye.jpg" alt="Paris">
-                        <p><strong>TSUN Mascot Shirt</strong></p>
+                    <div v-for="p in products" :key="p.id" class="col-sm-3 p-3 bg-white">
+                        <router-link  target="_blank" :to="{ path: 'detail_product', query: { id: p.id }}">
+                            <div class="thumbnail">
+                                <img :src="p.imgs[0]" alt="Paris">
+                                <p><strong>{{ p.title }}</strong></p>
+                            </div>
+                        </router-link>
                     </div>
-                </div>
-                <div class="col-sm-3 p-3 bg-white">
-                    <div class="thumbnail">
-                        <img src="img/Sweater Tiedye.jpg" alt="Paris">
-                        <p><strong>TSUN Mascot Shirt</strong></p>
-                    </div>
-                </div>
-                <div class="col-sm-3 p-3 bg-white">
-                    <div class="thumbnail">
-                        <img src="img/Sweater Tiedye.jpg" alt="Paris">
-                        <p><strong>TSUN Mascot Shirt</strong></p>
-                    </div>
-                </div>
-                <div class="col-sm-3 p-3 bg-white">
-                    <div class="thumbnail">
-                        <img src="img/Sweater Tiedye.jpg" alt="Paris">
-                        <p><strong>TSUN Mascot Shirt</strong></p>
-                    </div>
-                </div>
             </div>
             <br>
-            <div class="row">
-                <div class="col-sm-3 p-3 bg-white">
-                    <div class="thumbnail">
-                        <img src="img/Sweater Tiedye.jpg" alt="Paris">
-                        <p><strong>TSUN Mascot Shirt</strong></p>
-                    </div>
-                </div>
-                <div class="col-sm-3 p-3 bg-white">
-                    <div class="thumbnail">
-                        <img src="img/Sweater Tiedye.jpg" alt="Paris">
-                        <p><strong>TSUN Mascot Shirt</strong></p>
-                    </div>
-                </div>
-                <div class="col-sm-3 p-3 bg-white">
-                    <div class="thumbnail">
-                        <img src="img/Sweater Tiedye.jpg" alt="Paris">
-                        <p><strong>TSUN Mascot Shirt</strong></p>
-                    </div>
-                </div>
-                <div class="col-sm-3 p-3 bg-white">
-                    <div class="thumbnail">
-                        <img src="img/Sweater Tiedye.jpg" alt="Paris">
-                        <p><strong>TSUN Mascot Shirt</strong></p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-            </div>
-            <br>
-            <div class="row">
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-            </div>
-            <br>
-            <div class="row">
-
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-                <div class="col-sm-3 p-3 bg-white">1 of 2</div>
-            </div>
         </div>
 
         <ul class="pagination" style="margin-left: 400px; ">
@@ -167,8 +104,53 @@
 import api from "@/api/base"
 export default {
     data: () => ({
-
+        products: []
    }),
-   
+    created () {
+    api.get('/products')
+        .then(r => {
+        this.products = r.data.products
+    })
+    },
+    methods: {
+        get_all: function () {
+            api.get('/products')
+                .then(r => {
+                this.products = r.data.products
+            })
+        },
+        get_tshirt: function () {
+            api.get('/products?category=tshirt')
+                .then(r => {
+                this.products = r.data.products
+            })
+        },
+        get_hoodie: function () {
+            api.get('/products?category=hoodie')
+                .then(r => {
+                this.products = r.data.products
+            })
+        },
+        get_quan: function () {
+            api.get('/products?category=quan')
+                .then(r => {
+                this.products = r.data.products
+            })
+        },
+        get_phukien: function () {
+            api.get('/products?category=phukien')
+                .then(r => {
+                this.products = r.data.products
+            })
+        },
+        get_aokhoac: function () {
+            api.get('/products?category=aokhoac')
+                .then(r => {
+                this.products = r.data.products
+            })
+        },
+
+   }
+
 }
 </script>
